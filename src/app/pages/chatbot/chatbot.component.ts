e8c1d4afe26afb948f779470baf4a0b2e6d86b7f
Kyle Pharoah <pharoahkyle@gmail.com>
2023-10-10 21:35:34 +0200

    char counter

--- src/app/pages/chatbot/chatbot.component.ts
+++ src/app/pages/chatbot/chatbot.component.ts
@@ -16,5 +16,21 @@ export class ChatbotComponent implements OnInit {
     this.starActive = !this.starActive;
     localStorage.setItem('starActive', this.starActive.toString());
   }
+
+  characterCount: number = 0;
+
+  updateCharacterCount(event: Event): void {
+      const inputElement = event.target as HTMLInputElement;
+      this.characterCount = inputElement.value.length;
+      const charCountElement = document.getElementById('char-count');
+      if (charCountElement) {
+          charCountElement.innerText = `${this.characterCount} / 1000`;
+      }
+  }
+  
+
+    sendMessage(): void {
+        
+    }
 }
 

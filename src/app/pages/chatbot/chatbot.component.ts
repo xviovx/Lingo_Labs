37ae7c01b06f84cdb0fd414517c9444b9f1654df
Kyle Pharoah <pharoahkyle@gmail.com>
2023-10-10 21:16:27 +0200

    chatbot: chat containers

--- src/app/pages/chatbot/chatbot.component.ts
+++ src/app/pages/chatbot/chatbot.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chatbot',
   templateUrl: './chatbot.component.html',
   styleUrls: ['./chatbot.component.css'],
 })
-export class ChatbotComponent{
-  
+export class ChatbotComponent implements OnInit {
+  starActive = false;
+
+  ngOnInit(): void {
+    this.starActive = localStorage.getItem('starActive') === 'true';
+  }
+
+  toggleStar(): void {
+    this.starActive = !this.starActive;
+    localStorage.setItem('starActive', this.starActive.toString());
+  }
 }
 

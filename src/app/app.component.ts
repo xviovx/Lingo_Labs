48ab635cb4353c6547a26b1cbf64c285c8e541bc
Kyle Pharoah <pharoahkyle@gmail.com>
2023-10-14 23:12:13 +0200

    some touch-ups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,50 +8,49 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent {
-  getGreeting(): string {
-    switch (this.router.url) {
-      case '/home':
-        return 'Welcome, Gringo! 👋🏻';
-      case '/past-exercises':
-        return 'View your Past Exercises here!';
-      case '/book-lesson':
-        return 'Ready to book a new Lesson?';
-      case '/stats':
-        return 'Check out your Stats!';
-      case '/chatbot':
-        return 'Chat with our Bot!';
-      default:
-        return '';
+export class AppComponent implements AfterViewInit {
+    getGreeting(): string {
+        switch (this.router.url) {
+            case '/home':
+                return 'Welcome, Gringo! 👋🏻';
+            case '/past-exercises':
+                return 'View your Past Exercises here!';
+            case '/book-lesson':
+                return 'Ready to book a new Lesson?';
+            case '/stats':
+                return 'Check out your Stats!';
+            case '/chatbot':
+                return 'Chat with our Bot!';
+            default:
+                return '';
+        }
     }
-  }
-  
-  sidenavHovered = false;
 
-  onSidenavHover(hovered: boolean) {
-    this.sidenavHovered = hovered;
-  }
+    sidenavHovered = false;
 
-  title = 'Lingo_Labs';
+    onSidenavHover(hovered: boolean) {
+        this.sidenavHovered = hovered;
+    }
 
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
+    title = 'Lingo_Labs';
 
-  constructor(
-    public observer: BreakpointObserver,
-    public router: Router
-    ) {}
+    @ViewChild(MatSidenav)
+    sidenav!: MatSidenav;
 
-  ngAfterViewInit() {
-    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
-      if (res.matches) {
-        this.sidenav.mode = 'over';
-        this.sidenav.close();
-      } else {
-        this.sidenav.mode = 'side';
-        this.sidenav.open();
-      }
-    });
-  }
+    constructor(
+      public observer: BreakpointObserver,
+      public router: Router,
+  ) {}
+
+    ngAfterViewInit() {
+        this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+            if (res.matches) {
+                this.sidenav.mode = 'over';
+                this.sidenav.close();
+            } else {
+                this.sidenav.mode = 'side';
+                this.sidenav.open();
+            }
+        });
+    }
 }

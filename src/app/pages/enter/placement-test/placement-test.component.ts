8567402262cc5fb1168e152e523dac1d6e7c86ae
Kyle <pharoahkyle@gmail.com>
2023-10-25 00:10:04 +0200

    test-display front-end

--- src/app/pages/enter/placement-test/placement-test.component.ts
+++ src/app/pages/enter/placement-test/placement-test.component.ts
@@ -6,9 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./placement-test.component.scss']
 })
 export class PlacementTestComponent {
+  selectedOption: boolean = true;
+  isTestStarted: boolean = false;
   isModalOpen = false;
+  currentQuestionIndex = 0;
+  questions = [
+    {
+      text: "You ____________ all go to London next weekend",
+      options: ["Option A", "Option B", "Option C", "Option D"],
+      correctOption: "Option A"
+    },
+    // questions go here
+  ];
 
   toggleModal() {
     this.isModalOpen = !this.isModalOpen;
+  }
+
+  startTest() {
+    this.isModalOpen = false;
+    this.isTestStarted = true;
+  }
+
+  submitAnswer(answer: string) {
+    // test logic
+    if (answer === this.questions[this.currentQuestionIndex].correctOption) {
+      // correct answer logic
+    }
+    this.currentQuestionIndex++;
+    if (this.currentQuestionIndex >= this.questions.length) {
+      this.isTestStarted = false;
+      // test completion logic
+    }
   }
 }

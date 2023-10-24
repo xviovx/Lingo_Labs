ed79bb3b9b2dfcc53444c64d8214c33ba53276b1
Kyle <pharoahkyle@gmail.com>
2023-10-25 00:51:23 +0200

    updates to placement test

--- src/app/pages/enter/placement-test/placement-test.component.ts
+++ src/app/pages/enter/placement-test/placement-test.component.ts
@@ -6,18 +6,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./placement-test.component.scss']
 })
 export class PlacementTestComponent {
-  selectedOption: boolean = true;
-  isTestStarted: boolean = false;
+  selectedOption: string | null = null;
+  isTestStarted: boolean = true;
   isModalOpen = false;
   currentQuestionIndex = 0;
+  score = 0;
   questions = [
     {
-      text: "You ____________ all go to London next weekend",
-      options: ["Option A", "Option B", "Option C", "Option D"],
-      correctOption: "Option A"
+      text: "You _______ all go to London next weekend",
+      options: ["are", "have", "can", "need"],
+      correctOption: "can"
     },
-    // questions go here
+    {
+      text: "We _______ go to the beach if it's raining.",
+      options: ["will", "would", "should", "won't"],
+      correctOption: "won't"
+    },
+    {
+      text: "He _______ finished his homework yet.",
+      options: ["hasn't", "don't", "hasn't been", "isn't"],
+      correctOption: "hasn't"
+    },
+    {
+      text: "She can sing _______.",
+      options: ["good", "best", "well", "betterly"],
+      correctOption: "well"
+    },
+    {
+      text: "I'm afraid of _______ in the dark.",
+      options: ["walks", "walked", "walking", "to walk"],
+      correctOption: "walking"
+    },
+    {
+      text: "She has been living here _______ 2010.",
+      options: ["since", "for", "during", "by"],
+      correctOption: "since"
+    },
+    {
+      text: "I _______ have some milk in my coffee.",
+      options: ["like", "likes", "would like", "liking"],
+      correctOption: "would like"
+    },
+    {
+      text: "Every student _______ wear a uniform.",
+      options: ["has to", "don't have to", "doesn't has to", "need"],
+      correctOption: "has to"
+    },
+    {
+      text: "She _______ her keys. She can't find them anywhere.",
+      options: ["loses", "losted", "has lost", "was lost"],
+      correctOption: "has lost"
+    },
+    {
+      text: "If I _______ you, I'd study more.",
+      options: ["am", "was", "were", "had been"],
+      correctOption: "were"
+    },
+    {
+      text: "I wish I _______ fly.",
+      options: ["can", "could", "canning", "will"],
+      correctOption: "could"
+    }
   ];
+
+  constructor() {
+    const previousScore = localStorage.getItem('score');
+    if (previousScore) {
+      this.score = +previousScore;
+    }
+
+    const previousAnswers = localStorage.getItem('answers');
+    if (previousAnswers) {
+      const answers = JSON.parse(previousAnswers);
+    }
+  }
 
   toggleModal() {
     this.isModalOpen = !this.isModalOpen;
@@ -29,14 +91,19 @@ export class PlacementTestComponent {
   }
 
   submitAnswer(answer: string) {
-    // test logic
+    localStorage.setItem(`question_${this.currentQuestionIndex}`, answer);
+    
     if (answer === this.questions[this.currentQuestionIndex].correctOption) {
-      // correct answer logic
+      this.score++;
     }
+
     this.currentQuestionIndex++;
+    
     if (this.currentQuestionIndex >= this.questions.length) {
       this.isTestStarted = false;
-      // test completion logic
+      localStorage.setItem('score', this.score.toString());
+    } else {
+      this.selectedOption = null;
     }
   }
 }
